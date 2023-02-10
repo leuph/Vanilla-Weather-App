d42f2f633a544d6902c4295463f0cce71bc1a1e3
Lena <Uphoff.Lena@gmx.de>
2023-02-10 20:08:32 +0100

    Calling JS to update weather API details

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
-alert("Hello World");
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#main-temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
+}
+
+let apiKey = "472f731f6bc31a7ff3af040te3ofbdf5";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}&units=metric`;
+
+console.log();
+axios.get(apiUrl).then(displayTemperature);
