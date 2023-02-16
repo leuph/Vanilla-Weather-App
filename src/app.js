bf14faa7a4c0bc79e5b538a56f49a70d2c191595
Lena <Uphoff.Lena@gmx.de>
2023-02-16 09:44:35 +0100

    Display the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ function handleSubmit(event) {
 }
 
 function formatDate(timestamp) {
-  let date = new Date(timestamp);
+  let date = new Date(timestamp * 1000);
   let hours = date.getHours();
   let minutes = date.getMinutes();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -29,27 +29,38 @@ function formatDate(timestamp) {
   return `${days[day]} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = date.getDay();
+  return days[day];
+}
+
 function displayForecast(response) {
   console.log(response.data);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
 
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6)
+      forecastHTML =
+        forecastHTML +
+        `
               <div class="col-2">
-                <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-date">
+                ${formatDay(forecastDay.time)}</div>
                 <img
-                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/scattered-clouds-night.png"
+                  src="${forecastDay.condition.icon_url}"
                   alt="partly cloudy icon"
                   width="42"
                 />
                 <div class="weather-forecast-temperatures">
-                  <span class="weather-forecast-temperature-max">18°</span>
-                  <span class="weather-forecast-temperature-min">12°</span>
+                  <span class="weather-forecast-temperature-max">
+                  ${Math.round(forecastDay.temperature.maximum)}°</span>
+                  <span class="weather-forecast-temperature-min">
+                  ${Math.round(forecastDay.temperature.minimum)}°</span>
                 </div>
               </div>`;
   });
@@ -79,7 +90,7 @@ function displayTemperature(response) {
   windElement.innerHTML = response.data.wind.speed;
 
   let dateElement = document.querySelector("#date");
-  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  dateElement.innerHTML = formatDate(response.data.time);
 
   getForecast(response.data.coordinates);
 }
